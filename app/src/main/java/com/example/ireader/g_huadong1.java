8f2f3cccb4bda07220323bce9914be606f8383de
799413359 <799413359@qq.com>
2019-07-02 20:41:04 +0800

    滑动图片

---
+++ app/src/main/java/com/example/ireader/g_huadong1.java
@@ -0,0 +1,14 @@
+package com.example.ireader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class g_huadong1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.g_huadong1_activity);
+    }
+}
---
+++ app/src/main/java/com/example/ireader/g_huadong2.java
@@ -0,0 +1,14 @@
+package com.example.ireader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class g_huadong2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.g_huadong2_activity);
+    }
+}
