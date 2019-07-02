561ae6f387a1205341edb99cd74997094cc074d9
799413359 <799413359@qq.com>
2019-07-02 21:55:35 +0800

    滑动图片

--- app/src/main/java/com/example/ireader/g_resou.java
+++ app/src/main/java/com/example/ireader/g_resou.java
@@ -9,19 +9,62 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ScrollView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.os.Bundle;
+import android.view.Window;
+import java.util.ArrayList;
 public class g_resou extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private ArrayList<View> pageview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.g_resou_acticity);
 
+        viewPager=(ViewPager)findViewById(R.id.g_hua);
 
+        LayoutInflater inflater =getLayoutInflater();
+        View view1 = inflater.inflate(R.layout.g_huadong1_activity, null);
+        View view2 = ((LayoutInflater) inflater).inflate(R.layout.g_huadong2_activity, null);
+        View view3 = inflater.inflate(R.layout.g_huadong3_activity, null);
+
+
+
+        pageview =new ArrayList<View>();
+        pageview.add(view1);
+        pageview.add(view2);
+        pageview.add(view3);
+
+        PagerAdapter mPagerAdapter=new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return pageview.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(@NonNull View arg0, @NonNull Object arg1) {
+                return arg0==arg1;
+            }
+            public void destroyItem(View arg0,int arg1,Object arg2){
+                ((ViewPager)arg0).removeView(pageview.get(arg1));
+            }
+            public Object instantiateItem(View arg0,int arg1){
+                ((ViewPager)arg0).addView(pageview.get(arg1));
+                return pageview.get(arg1);
+            }
+        };
+        viewPager.setAdapter(mPagerAdapter);
 
 
         ScrollView scrollView=(ScrollView)findViewById(R.id.g_scroll1);
